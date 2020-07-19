e70198783c7979e5575d5f2802e0e28e9aa2447d
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-07-19 12:29:21 +0300

    update equality

--- equality_operators/js/script.js
+++ equality_operators/js/script.js
@@ -1,5 +1,37 @@
 'use strict';
 
-let company = prompt('Какая компания создала JavaScript?', '');
+// let company = prompt('Какая компания создала JavaScript?', '');
 
-company == 'Netscape' ? alert('Верно!') : alert('Неправильно.');
+// company == 'Netscape' ? alert('Верно!') : alert('Неправильно.');
+
+// const questionMessage = prompt('Какое официальное название JavaScript?');
+// if (questionMessage === 'ECMAScript') {
+//   alert('Верно!');
+// } else {
+//   alert('Не знаете? ECMAScript!');
+// }
+
+// const num = prompt('Введите число');
+// if (num > 0) {
+//   alert(1);
+// } else if (num < 0) {
+//   alert(-1);
+// } else {
+//   alert(0);
+// }
+
+// let result;
+
+// result = a + b < 4 ? 'мало' : 'много';
+const login = prompt('Введите логин');
+let message;
+message =
+  login === 'Сотрудник'
+    ? 'Привет'
+    : login === 'Директор'
+    ? 'Здравствуйте'
+    : login === ''
+    ? 'Нет логина'
+    : '';
+
+alert(message);
